3ade1f932f5c0cb1bfaa23fbb4dabd35dd84cb45
juanjo <juanmacmar49mail.com>
2024-09-12 11:13:16 -0500

    se agrego un comentario

---
+++ Arrays.java
@@ -0,0 +1,29 @@
+
+package arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int numero =0;
+      
+        int number[]= {50, 70, 75,12, 45};
+        for (int i=0; i<=number.length -1  ; i++ ){
+            if (number[i] > numero){
+                numero = number[i];
+                
+            }
+        }   
+        System.out.println("el numero mayor es: " + numero);
+         
+        for (int i=0; i<=number.length -1  ; i++ ){
+            if (number[i] < numero){
+             numero = number[i];
+               //git patacon 
+            }
+        }
+        System.out.println("el numero menor es: " + numero);
+    }
+}  
+    
+
+
+
